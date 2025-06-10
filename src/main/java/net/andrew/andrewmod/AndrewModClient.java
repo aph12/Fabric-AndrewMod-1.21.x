ccd48ce62440610aee7281780feeba52e3678d9a
aph12 <aph6021@gmail.com>
2025-06-10 17:52:30 -0400

    Initial commit

---
+++ src/main/java/net/andrew/andrewmod/AndrewModClient.java
@@ -0,0 +1,10 @@
+package net.andrew.andrewmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AndrewModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
